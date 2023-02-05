4e664423ee2b511474dc32882b907120defeff7d
Ruba Sbeih <rsasbeih@gmail.com>
2023-02-05 15:24:58 +0200

    Added a filter option to filter books by year read

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
 import BookItem from "./BookItem";
+import BooksFilter from "./BooksFilter";
 import './Books.css';
 import Card from "../UI/Card";
 const Books = (props) => {
-        return (<Card className="books">
-                {props.books.map(book => <BookItem key={book.id} title={book.title} date={book.date} pages={book.pages} />)}
-        </Card>);
+        const [filteredYear, setFilteredYear] = useState('2020');
+        const filterChangeHandler = selectedYear => {
+                setFilteredYear(selectedYear);
+        }
+        const filteredBooks = props.books.filter(book => {
+                return book.date.getFullYear().toString() === filteredYear;
+        });
+        return (
+                <div>
+                        <Card className="books">
+                                <BooksFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                                {filteredBooks.length === 0 ? <p className="books__message">You didn't read any books this year.</p> : filteredBooks.map(book => <BookItem key={book.id} title={book.title} date={book.date} pages={book.pages} />)}
+
+                        </Card>
+                </div>);
 }
 
 export default Books;
