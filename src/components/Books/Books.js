d37beb9f230e654be27851491e4639eac16903d0
Ruba Sbeih <rsasbeih@gmail.com>
2023-02-06 13:10:15 +0200

    Added a filter option to be able to see which month you read the most

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -3,6 +3,7 @@ import BookItem from "./BookItem";
 import BooksFilter from "./BooksFilter";
 import './Books.css';
 import Card from "../UI/Card";
+import BookChart from './BookChart';
 const Books = (props) => {
         const [filteredYear, setFilteredYear] = useState('2020');
         const filterChangeHandler = selectedYear => {
@@ -15,6 +16,7 @@ const Books = (props) => {
                 <div>
                         <Card className="books">
                                 <BooksFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                                <BookChart books={filteredBooks} />
                                 {filteredBooks.length === 0 ? <p className="books__message">You didn't read any books this year.</p> : filteredBooks.map(book => <BookItem key={book.id} title={book.title} date={book.date} pages={book.pages} />)}
 
                         </Card>
